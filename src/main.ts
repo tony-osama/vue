27853f7e9e8931e345f0930e0db7e36e6ac41044
tony-osama <tonyosama1546@gmail.com>
2025-04-05 07:46:05 +0200

    Add Bootstrap dependencies and styles to the project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// ...Bootstrap Files...
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const app = createApp(App)
 
